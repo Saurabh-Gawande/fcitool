f62255b868b0120f1862de17d57ee03acfeafa1a
AK-0906 <114081849+AK-0906@users.noreply.github.com>
2023-08-14 12:42:42 +0530

    FCI

--- Frontend/src/pages/Mode1/Template.jsx
+++ Frontend/src/pages/Mode1/Template.jsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState, useEffect } from "react";
+import React, {  useState } from "react";
 import Sidenav from "./sidenav";
 import * as XLSX from "xlsx";
 import { saveAs } from "file-saver";
