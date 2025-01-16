2eb2982627f6b22d8a961c57c81387b1965dac3c
Saurabh Gawande <saurabhgawande2016@gmail.com>
2025-01-16 14:43:36 +0530

    changed the css

--- Frontend/src/pages/Mode1/sidenav.jsx
+++ Frontend/src/pages/Mode1/sidenav.jsx
@@ -3,8 +3,25 @@ import "./Navbar.css";
 
 function Sidenav() {
   return (
-    <nav className="navbar">
-      <ul className="navbar-list">
+    <nav
+      className="navbar"
+      style={{ display: "grid", gridTemplateColumns: "auto 1fr", gap: 10 }}
+    >
+      <img
+        src="static/img/logo.png"
+        alt="logo"
+        style={{ width: 160, height: 43, marginLeft: 20 }}
+      />
+      <ul
+        className="navbar-list"
+        style={{
+          display: "grid",
+          justifyContent: "center",
+          gap: 10,
+          fontFamily:
+            ' -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"',
+        }}
+      >
         <li>Plan Optimizer</li>
       </ul>
     </nav>
